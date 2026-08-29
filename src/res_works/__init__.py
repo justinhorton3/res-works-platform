21ed639ba9823969aa9f0eb519bd6987173e9478
justinhorton3 <justinhorton3@gmail.com>
2026-08-29 15:10:30 -0500

    feat: add explicit plan geometry

--- src/res_works/__init__.py
+++ src/res_works/__init__.py
@@ -8,5 +8,6 @@ from .pdf_render import render_pdf_pages
 from .validation import evaluate_requirement, evaluate_requirements
 from .rule_catalog import load_requirements
 from .reports import build_validation_report
+from .geometry import validate_geometry
 
-__all__ = ["ProjectManifest", "ProjectRepository", "ingest_artifact", "inventory_pdf", "render_pdf_pages", "evaluate_requirement", "evaluate_requirements", "load_requirements", "build_validation_report"]
+__all__ = ["ProjectManifest", "ProjectRepository", "ingest_artifact", "inventory_pdf", "render_pdf_pages", "evaluate_requirement", "evaluate_requirements", "load_requirements", "build_validation_report", "validate_geometry"]
